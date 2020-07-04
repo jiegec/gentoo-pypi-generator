d457f50fdb7b94218887bcb35e775d527bb70d99
Jiajie Chen <jiegec@qq.com>
2020-07-05 07:42:32 +0800

    Generate for simple projects

--- generator.py
+++ generator.py
@@ -3,8 +3,10 @@ import sys
 import json
 import requests
 import re
+from collections import defaultdict
+from pathlib import Path
 
-supported_python_versions = ['3.5', '3.6']
+supported_python_versions = ['3.6', '3.7']
 
 def get_project_python_versions(project):
     classifiers = project['info']['classifiers']
@@ -16,18 +18,24 @@ def get_project_python_versions(project):
                 break
     return res
 
-def get_depend(project):
+def get_iuse_and_depend(project):
     requires = project['info']['requires_dist']
-    res = []
+    simple = []
+    uses = defaultdict(list)
     for req in requires:
         match = re.match("(.+) ; extra == '(.+)'", req)
         if match:
             name = match.group(1)
             use = match.group(2)
-            res.append('{}? ( dev-python/{} )'.format(use, name))
+            uses[use].append('dev-python/{}'.format(name))
         else:
-            res.append('dev-python/{}'.format(req))
-    return 'RDEPEND="' + '\n\t'.join(res) + '"'
+            simple.append('dev-python/{}'.format(req))
+
+    use_res = []
+    for use in uses:
+        use_res.append('{}? ( {} )'.format(use, '\n\t\t'.join(uses[use])))
+    iuse = 'IUSE="{}"'.format(" ".join(uses.keys()))
+    return iuse + '\n' + 'RDEPEND="' + '\n\t'.join(simple + use_res) + '"'
 
 def main():
     parser = argparse.ArgumentParser()
@@ -41,11 +49,32 @@ def main():
         resp = requests.get("https://pypi.org/pypi/{}/json".format(package))
         body = json.loads(resp.content)
 
-        print('Python versions', get_project_python_versions(body))
+        versions = get_project_python_versions(body)
+        compat = ' '.join(['python' + version.replace('.','_') for version in versions])
+        print('Python versions', versions)
         print('Homepage', body['info']['home_page'])
+        print('Description', body['info']['summary'])
         print('License', body['info']['license'])
         print('Version', body['info']['version'])
-        print('Depend', get_depend(body))
+        print('IUSE and Depend', get_iuse_and_depend(body))
+
+        path = Path(args.repo) / "dev-python" / package / "{}-{}.ebuild".format(package, body['info']['version'])
+        print('Writing to', path)
+        with path.open('w') as f:
+            content = '# Copyright 1999-2020 Gentoo Authors\n'
+            content += '# Distributed under the terms of the GNU General Public License v2\n\n'
+            content += 'EAPI=7\n\n'
+            content += 'PYTHON_COMPAT=( {} )\n\n'.format(compat)
+            content += 'inherit distutils-r1\n\n'
+            content += 'DESCRIPTION="{}"\n'.format(body['info']['summary'])
+            content += 'SRC_URL="mirror://pypi/${PN:0:1}/${PN}/${PN}-${PV}.tar.gz"\n'
+            content += 'HOMEPAGE="{}"\n\n'.format(body['info']['home_page'])
+            content += 'LICENSE="{}"\n'.format(body['info']['license'])
+            content += 'SLOT="0"\n'
+            content += 'KEYWORDS="amd64"\n\n'
+            content += get_iuse_and_depend(body)
+
+            f.write(content)
 
 if __name__ == "__main__":
     main()
