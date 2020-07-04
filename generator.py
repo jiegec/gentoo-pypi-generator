1457fe49d0da8eec4b2f09458cae7206025fe0fc
Jiajie Chen <jiegec@qq.com>
2020-07-04 19:30:53 +0800

    Add argparse

--- generator.py
+++ generator.py
@@ -1,35 +1,11 @@
-import getopt
+import argparse
 import sys
 
-def usage():
-    print('{}: Generate ebuild for packages on PyPI'.format(sys.argv[0]))
-    print('\t-h, --help: display help')
-    print('\t-v, --verbose: enable verbose logging')
-    print('\t-r, --repo=: set repo directory (default: ../gentoo-localrepo)')
-
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hr:v", ["help", "repo=", "verbose"])
-    except getopt.GetoptError as err:
-        print(err)
-        usage()
-        sys.exit(1)
-
-    repo = "../gentoo-localrepo"
-    verbose = False
-    for o, a in opts:
-        if o in ("-v", "verbose"):
-            verbose = True
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o in ("-r", "--repo"):
-            repo = a
-        else:
-            print("unhandled option: {} {}".format(o, a))
-            sys.exit(1)
-
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--repo', help='set repo directory', default='../gentoo-localrepo')
+    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose logging')
+    args = parser.parse_args()
 
 if __name__ == "__main__":
     main()
