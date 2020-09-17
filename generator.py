a3c9f6404da684a66685905d9fe6ce6b99b91694
jiegec <noc@jiegec.ac.cn>
2020-09-17 22:56:47 +0800

    Handle renames in dependency

--- generator.py
+++ generator.py
@@ -24,6 +24,8 @@ missing_packages = set()
 def get_package_name(package):
     if package in exceptions:
         return exceptions[package]
+    elif package in renames:
+        return renames[package]
     else:
         if not package in existing_packages:
             print("Package '%s' does not exist" % package)
