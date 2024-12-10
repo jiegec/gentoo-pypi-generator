da2c7cb59c4855546d3fe33e6a7e50f66044c2e9
Yiyang Wu <xgreenlandforwyy@gmail.com>
2024-12-10 10:39:09 +0800

    Always fetch and generate manifest, to determine DISTUTILS_USE_PEP517
    Signed-off-by: Yiyang Wu <xgreenlandforwyy@gmail.com>

--- generator.py
+++ generator.py
@@ -220,8 +220,7 @@ def generate(package_pypi, args):
 
         f.write(content)
 
-    if args.manifest:
-        os.system('cd %s && pkgdev manifest' % (dir))
+    os.system('cd %s && pkgdev manifest' % (dir))
         
     if package_pypi in missing_packages:
         missing_packages.remove(package_pypi)
@@ -236,7 +235,6 @@ def main():
     parser.add_argument('-r', '--repo', help='set repo directory', default='../gentoo-localrepo')
     parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose logging')
     parser.add_argument('-R', '--recursive', action='store_true', help='generate ebuild recursively')
-    parser.add_argument('-m', '--manifest', action='store_true', help='run "pkgdev manifest" after generation')
     parser.add_argument('-a', '--all-repo', action='store_true', help='search all repos for existing packages including overlays')
     parser.add_argument('packages', nargs='+')
     args = parser.parse_args()
